13a9ce9b0adcf875b37b1c7419cf37d8df6bbcd6
budzikt <budzikt@gmail.com>
2016-07-16 17:10:51 +0200

    Ab

---
+++ Wc3Template/scripts/nav.js
@@ -0,0 +1,8 @@
+function w3_open() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
