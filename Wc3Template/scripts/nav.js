b0d372e722818c3d61ef1451627542ba9349d1e1
budzikt <budzikt@gmail.com>
2016-07-17 00:53:40 +0200

    show/hide enhancment

--- Wc3Template/scripts/nav.js
+++ Wc3Template/scripts/nav.js
@@ -1,8 +1,45 @@
 function w3_open() {
-    document.getElementById("mySidenav").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
+    replaceClass(document.getElementById("mySidenav"), "w3-hide", "w3-show");
+    replaceClass(document.getElementById("myOverlay"), "w3-hide", "w3-show");
+ 
 }
+
 function w3_close() {
-    document.getElementById("mySidenav").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
+    replaceClass(document.getElementById("mySidenav"), "w3-show", "w3-hide");
+    replaceClass(document.getElementById("myOverlay"), "w3-show", "w3-hide");
+}
+
+    
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
+
+function replaceClass(element, classToDrop, classToAdd)
+{
+    if(hasClass(element, classToDrop)){
+        removeClass(element, classToDrop);
+        addClass(element, classToAdd);
+    }
+    else{
+        addClass(element, classToAdd);
+    }
 }
